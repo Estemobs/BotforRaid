3b520896a674984590aedecdc0067b36ee5c963e
estemobs#9212 <57631804+Estemobs@users.noreply.github.com>
2020-10-15 09:45:15 +0200

    Update anarchy raid.py

--- anarchy raid.py
+++ anarchy raid.py
@@ -5,12 +5,12 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='r')
 
+liste blanche= []
+
 @bot.command()
 async def raid(ctx):
-    role = discord.utils.get(ctx.guild.roles, name="directeur adj")
-    await ctx.author.add_roles(role)
+    
 
 
 bot.run('token')
 
-liste noire = [616303571687702569,458683322130890792,304327970460270592]
